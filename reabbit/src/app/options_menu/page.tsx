e74aa7020159ecb88acb481cda49cac39a7f5d6e
nihaljames <52252893+nihaljames@users.noreply.github.com>
2025-01-22 21:32:14 -0800

    Update page.tsx

--- reabbit/src/app/options_menu/page.tsx
+++ reabbit/src/app/options_menu/page.tsx
@@ -1,11 +1,19 @@
-import Link from "next/link"
-import './options.css'
+import Link from "next/link";
+import './options.css';
 
 export default function OptionsPage() {
     return (
         <div id="optionsPage">
+            <div id="titleSection">
+                <div id="title">BEEZY</div>
+                <img 
+                    id="logo" 
+                    src="/beelogoBW.png" 
+                    alt="Beezy Logo" 
+                />
+            </div>
             <Link href={'/freelancer_page'} className="bttn">Freelancer</Link>
             <Link href={'/client_page'} className="bttn">Client</Link>
         </div>
-    )
+    );
 }
